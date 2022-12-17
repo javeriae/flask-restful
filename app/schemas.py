198d939609437c3cf7633f7674dda695f26b6e64
Javeria Ejaz <javeriae456@gmail.com>
2022-12-17 14:57:31 +0500

    Added Book Model with Schema & Serializer

--- app/schemas.py
+++ app/schemas.py
@@ -23,3 +23,28 @@ class UserSchema(Schema):
         data['username'] = data.get('username', '').strip()
         data['password'] = data.get('password', '').strip()
         return data
+
+
+class BookSchema(Schema):
+    """
+    Schemas for Book model
+    """
+    title = fields.Str(validate=validate.Length(min=1))
+    description = fields.Str(validate=validate.Length(min=1))
+    pages = fields.Integer(
+        strict=True, required=True, validate=[validate.Range(min=1, error="Value must be greater than 0")]
+    )
+    published_on = fields.DateTime(required=True, format='%Y-%m-%d')
+
+    @pre_load
+    def process_input(self, data, **kwargs):
+        """
+        Pre-processing the title and description to strip extra whitespaces
+        Args:
+            data(dict): data provided by user.
+
+        Returns: dict
+        """
+        data['title'] = data.get('title', '').strip()
+        data['description'] = data.get('description', '').strip()
+        return data
--- app/serializers.py
+++ app/serializers.py
@@ -2,6 +2,7 @@
 Serializers for all application model.
 """
 
+
 def user_serializer(user):
     """
     Serialize the fields of user.
@@ -12,4 +13,19 @@ def user_serializer(user):
     return {
         'username': user.username,
         'password': user.password
+    }
+
+
+def book_serializer(book):
+    """
+    Serialize the fields of book.
+
+    Arguments:
+        book: Book object
+    """
+    return {
+        'title': book.title,
+        'description': book.description,
+        'pages': book.pages,
+        'published_on': book.published_on.strftime('%Y-%m-%d')
     }
