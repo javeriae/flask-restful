3de0178ae6d7283b8c6313ef1249fdd84a0409a7
Javeria Ejaz <javeriae456@gmail.com>
2022-12-17 15:23:16 +0500

    Added CRUD views for Book Model

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ Routes for all application views.
 from flask import Blueprint
 from flask_restful import Api
 
-from .views import Index, UserItems
+from .views import BookItem, BookItems, Index, UserItems
 
 home_bp = Blueprint('home_bp', __name__)
 
@@ -12,3 +12,5 @@ home_api = Api(home_bp)
 
 home_api.add_resource(Index, '/')
 home_api.add_resource(UserItems, '/users')
+home_api.add_resource(BookItems, '/books')
+home_api.add_resource(BookItem, '/books/<book_id>')
--- app/views.py
+++ app/views.py
@@ -1,14 +1,16 @@
 """
 Application Views.
 """
+from datetime import datetime
+
 from flask import request
 from flask_restful import Resource
 from werkzeug.security import generate_password_hash
 
 from app.app import basic_auth, db
-from app.models import User
-from app.schemas import UserSchema
-from app.serializers import user_serializer
+from app.models import Book, User
+from app.schemas import BookSchema, UserSchema
+from app.serializers import book_serializer, user_serializer
 
 
 class Index(Resource):
@@ -70,4 +72,145 @@ class UserItems(Resource):
             'message': 'Record added successfully.',
             'resource_id': user.username,
             'status_code': 201
+        }
+
+
+class BookItems(Resource):
+    """
+    Views to retrieve book objects or add new object.
+    """
+    @basic_auth.login_required
+    def get(self):
+        """
+        Method to retrieve the list of all books.
+
+        Returns: dict
+        """
+        books = Book.query.all()
+        results = [book_serializer(book) for book in books]
+        return {
+            'count': len(results),
+            'objects': results,
+        }
+
+    @basic_auth.login_required
+    def post(self):
+        """
+        Method to create a new user.
+
+        Returns: dict
+        """
+        book_schema = BookSchema()
+        errors = book_schema.validate(request.json)
+
+        if errors:
+            return {
+                       'error_msg': 'Invalid data.',
+                       'error_code': 'BAD_REQUEST',
+                       'status_code': 400
+                   }, 400
+
+        book = Book(
+            title=request.json.get('title'),
+            description=request.json.get('description'),
+            pages=request.json.get('pages'),
+            published_on=datetime.strptime(request.json.get('published_on'), '%Y-%m-%d').date()
+        )
+        db.session.add(book)
+        db.session.commit()
+
+        return {
+            'message': 'Record added successfully.',
+            'resource_id': book.id,
+            'status_code': 201
+        }
+
+
+class BookItem(Resource):
+    """
+    Views to retrieve a single book object or edit a object.
+    """
+    @basic_auth.login_required
+    def get(self, book_id):
+        """
+        Method to retrieve data for a single book object.
+
+        Args:
+        book_id(int): Book id value provided by user
+
+        Returns: dict
+        """
+        book = Book.query.get(book_id)
+
+        if not book:
+            return {
+               'error_msg': 'Resource not found.',
+               'error_code': 'NOT_FOUND',
+               'status_code': 404
+            }, 404
+
+        return book_serializer(book)
+
+    @basic_auth.login_required
+    def put(self, book_id):
+        """
+        Method to update data for a book object.
+
+        Args:
+        book_id(int): Book id value provided by user
+
+        Returns: dict
+        """
+        book_schema = BookSchema()
+        errors = book_schema.validate(request.json)
+
+        if errors:
+            return {
+               'error_msg': 'Invalid data.',
+               'error_code': 'BAD_REQUEST',
+               'status_code': 400
+            }, 400
+
+        book = Book.query.get(book_id)
+        if book:
+            book.title = request.json.get('title')
+            book.description = request.json.get('description')
+            book.pages = request.json.get('pages')
+            book.published_on = datetime.strptime(request.json.get('published_on'), '%Y-%m-%d').date()
+
+        db.session.add(book)
+        db.session.commit()
+
+        return {
+            'message': 'Record updated successfully.',
+            'resource_id': book.id,
+            'status_code': 204
+        }
+
+    @basic_auth.login_required
+    def delete(self, book_id):
+        """
+        Method to delete a single book object.
+
+        Args:
+        book_id(int): Book id value provided by user
+
+        Returns: dict
+        """
+        book = Book.query.get(book_id)
+
+        if not book:
+            return {
+                       'error_msg': 'Resource not found.',
+                       'error_code': 'NOT_FOUND',
+                       'status_code': 404
+                   }, 404
+
+        db.session.delete(book)
+        db.session.commit()
+
+        return {
+            'message': 'Record deleted successfully.',
+            'resource_id': book.id,
+            'status_code': 204
         }
